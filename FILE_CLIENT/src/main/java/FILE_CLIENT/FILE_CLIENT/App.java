384cf5735a72f99671134dc1bc4ff3262461bd24
EstebanMV96 <moyaco41@gmail.com>
2017-05-07 17:50:19 -0500

    Proyectos vacios

---
+++ FILE_CLIENT/src/main/java/FILE_CLIENT/FILE_CLIENT/App.java
@@ -0,0 +1,13 @@
+package FILE_CLIENT.FILE_CLIENT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
