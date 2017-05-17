4ee353a5169dc22fb98511641ae3354e2e720235
EstebanMV96 <moyaco41@gmail.com>
2017-05-17 14:22:06 -0500

    Agregar modulo cifrado

---
+++ FILE_CLIENT/src/main/java/control/Cifrador.java
@@ -0,0 +1,113 @@
+package control;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Cifrador {
+	
+	
+	private  SecretKeySpec key ;
+	
+	
+	public Cifrador() throws IOException
+	{
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("0 PARA CIFRAR O 1 PARA DECIFRAR");
+		int n=Integer.parseInt(br.readLine());
+		System.out.println("INTRODUZCA EL NOMBRE DEL ARCHIVO");
+		String nomArchivo=br.readLine();
+		System.out.println("INTRODUZCA LA CLAVE");
+		String llaveSimetrica = br.readLine();
+		key = new SecretKeySpec(llaveSimetrica.getBytes(), "AES");
+		if(n==0)
+		cifrar(nomArchivo);
+		else
+		des(nomArchivo);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		   
+	new Cifrador();
+		   
+		   //Comienzo a desencriptar
+//		   cipher.init(Cipher.DECRYPT_MODE, key);
+//		   byte[] datosDecifrados = cipher.doFinal(campoCifrado);
+//		   String mensaje_original = new String(datosDecifrados);
+//		   System.out.println(mensaje_original);
+//		  } catch (Exception e) {
+//		   e.printStackTrace();
+//		  }
+	}
+	
+	
+	
+	public void cifrar(String nomArch)
+	{
+		 try {
+			
+			Cipher cipher=Cipher.getInstance("AES");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			File archivo=new File(nomArch);
+			byte[] bytesArch=new byte[(int) archivo.length()];
+			FileInputStream fis=new FileInputStream(archivo);
+			fis.read(bytesArch);
+			fis.close();
+			String[] gu=nomArch.split("\\.");
+			byte[] campoCifrado = cipher.doFinal(bytesArch);
+			FileOutputStream fos=new FileOutputStream(new File(gu[0]+".protect"));
+			fos.write(campoCifrado);
+			fos.close();
+			System.out.println("ARCHIVO CIFRADO");
+		 }catch(Exception e)
+		 {
+			 e.printStackTrace();
+		 }
+	
+	}
+	
+	public void des(String nomArch)
+	{
+		try{
+			Cipher cipher=Cipher.getInstance("AES");
+			String[] f=nomArch.split("\\.");
+			File arch=new File(f[0]+".backup");
+			File arch1=new File(nomArch);
+			FileInputStream fis=new FileInputStream(arch1);
+			byte[] a=new byte[(int)arch1.length()];
+			fis.read(a);
+			fis.close();
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			byte[] datosDecifrados = cipher.doFinal(a);
+			FileOutputStream fos=new FileOutputStream(arch);
+			fos.write(datosDecifrados);
+			fos.close();
+			
+		}catch (Exception e) {
+		   e.printStackTrace();
+		  }
+	}
+
+ 
+
+}
---
+++ FILE_SERVER/src/main/java/control/Cifrador.java
@@ -0,0 +1,92 @@
+package control;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Cifrador {
+	
+	
+	private  SecretKeySpec key ;
+	
+	
+	public Cifrador() throws IOException
+	{
+		String llaveSimetrica = "96AO;9OyQlY4uSGa";
+		key = new SecretKeySpec(llaveSimetrica.getBytes(), "AES");
+
+	}
+	
+
+	
+	
+	
+	public void cifrar(String nomArch)
+	{
+		 try {
+			
+			Cipher cipher=Cipher.getInstance("AES");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			File archivo=new File(nomArch);
+			byte[] bytesArch=new byte[(int) archivo.length()];
+			FileInputStream fis=new FileInputStream(archivo);
+			fis.read(bytesArch);
+			fis.close();
+			String[] gu=nomArch.split("\\.");
+			byte[] campoCifrado = cipher.doFinal(bytesArch);
+			FileOutputStream fos=new FileOutputStream(new File(gu[0]+".protect"));
+			fos.write(campoCifrado);
+			fos.close();
+			System.out.println("ARCHIVO CIFRADO");
+		 }catch(Exception e)
+		 {
+			 e.printStackTrace();
+		 }
+	
+	}
+	
+	public void des(String nomArch)
+	{
+		try{
+			Cipher cipher=Cipher.getInstance("AES");
+			String[] f=nomArch.split("\\.");
+			File arch=new File(f[0]+".backup");
+			File arch1=new File(nomArch);
+			FileInputStream fis=new FileInputStream(arch1);
+			byte[] a=new byte[(int)arch1.length()];
+			fis.read(a);
+			fis.close();
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			byte[] datosDecifrados = cipher.doFinal(a);
+			FileOutputStream fos=new FileOutputStream(arch);
+			fos.write(datosDecifrados);
+			fos.close();
+			
+		}catch (Exception e) {
+		   e.printStackTrace();
+		  }
+	}
+
+ 
+
+}
